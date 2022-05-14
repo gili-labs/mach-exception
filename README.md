51d37356570ef9e100d7611daf1f1b6572a6e214
Gili Labs <gili.patrick.r@gili-labs.com>
2022-05-14 15:06:47 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-exceptions
+
+A description of this package.
---
+++ Sources/swift-exceptions/swift_exceptions.swift
@@ -0,0 +1,6 @@
+public struct swift_exceptions {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
